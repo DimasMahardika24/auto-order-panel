e791454c577d9f36aeef18ee0ffecb3fa3725709
DhikzxCloud <109904745+DimasMahardika24@users.noreply.github.com>
2026-03-16 01:01:36 +0700

    Update check.js

--- netlify/functions/check.js
+++ netlify/functions/check.js
@@ -1,4 +1,4 @@
-const CONFIG = require('./config');
+const CONFIG = require('./config.js');
 
 exports.handler = async function(event, context) {
     if (event.httpMethod !== "POST") return { statusCode: 405, body: "Method Not Allowed" };
